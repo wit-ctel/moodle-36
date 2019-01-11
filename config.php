f8de6c4f6e5d6faef553fc0364f7519e126cf743
Cathal O’Riordan <cathal@madein75.com>
2019-01-11 10:00:10 +0000

    added local dev settings section

--- config.php
+++ config.php
@@ -22,6 +22,76 @@ $CFG->wwwroot   = 'http://moodle-36.example.com:8080';
 $CFG->dataroot  = '/Users/Cathal/Work/vle/moodle-36_data';
 $CFG->admin     = 'admin';
 
+//=========================================================================
+// 7. SETTINGS FOR DEVELOPMENT SERVERS - not intended for production use!!!
+//=========================================================================
+//
+// Force a debugging mode regardless the settings in the site administration
+@error_reporting(E_ALL | E_STRICT); // NOT FOR PRODUCTION SERVERS!
+@ini_set('display_errors', '1');    // NOT FOR PRODUCTION SERVERS!
+$CFG->debug = (E_ALL | E_STRICT);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION SERVERS!
+$CFG->debugdisplay = 1;             // NOT FOR PRODUCTION SERVERS!
+//
+// You can specify a comma separated list of user ids that that always see
+// debug messages, this overrides the debug flag in $CFG->debug and $CFG->debugdisplay
+// for these users only.
+// $CFG->debugusers = '2';
+//
+// Prevent theme caching
+$CFG->themedesignermode = true; // NOT FOR PRODUCTION SERVERS!
+//
+// Enable verbose debug information during fetching of email messages from IMAP server.
+// $CFG->debugimap = true;
+//
+// Prevent JS caching
+$CFG->cachejs = false; // NOT FOR PRODUCTION SERVERS!
+//
+// Restrict which YUI logging statements are shown in the browser console.
+// For details see the upstream documentation:
+//   http://yuilibrary.com/yui/docs/api/classes/config.html#property_logInclude
+//   http://yuilibrary.com/yui/docs/api/classes/config.html#property_logExclude
+// $CFG->yuiloginclude = array(
+//     'moodle-core-dock-loader' => true,
+//     'moodle-course-categoryexpander' => true,
+// );
+// $CFG->yuilogexclude = array(
+//     'moodle-core-dock' => true,
+//     'moodle-core-notification' => true,
+// );
+//
+// Set the minimum log level for YUI logging statements.
+// For details see the upstream documentation:
+//   http://yuilibrary.com/yui/docs/api/classes/config.html#property_logLevel
+$CFG->yuiloglevel = 'debug';
+//
+// Prevent core_string_manager application caching
+// $CFG->langstringcache = false; // NOT FOR PRODUCTION SERVERS!
+//
+// When working with production data on test servers, no emails or other messages
+// should ever be send to real users
+// $CFG->noemailever = true;    // NOT FOR PRODUCTION SERVERS!
+//
+// Divert all outgoing emails to this address to test and debug emailing features
+// $CFG->divertallemailsto = 'root@localhost.local'; // NOT FOR PRODUCTION SERVERS!
+//
+// Except for certain email addresses you want to let through for testing. Accepts
+// a comma separated list of regexes.
+// $CFG->divertallemailsexcept = 'tester@dev.com, fred(\+.*)?@example.com'; // NOT FOR PRODUCTION SERVERS!
+//
+// Uncomment if you want to allow empty comments when modifying install.xml files.
+// $CFG->xmldbdisablecommentchecking = true;    // NOT FOR PRODUCTION SERVERS!
+//
+// Since 2.0 sql queries are not shown during upgrade by default.
+// Please note that this setting may produce very long upgrade page on large sites.
+// $CFG->upgradeshowsql = true; // NOT FOR PRODUCTION SERVERS!
+//
+// Add SQL queries to the output of cron, just before their execution
+// $CFG->showcronsql = true;
+//
+// Force developer level debug and add debug info to the output of cron
+// $CFG->showcrondebugging = true;
+//
+
 $CFG->directorypermissions = 0777;
 
 require_once(__DIR__ . '/lib/setup.php');
